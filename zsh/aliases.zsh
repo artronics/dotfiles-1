3a4901c1df9caf9c4e8ab18e360e21a93f9f86fe
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-04-29 21:19:19 +0000

    add vimrc.local tmux from previous. load zsh doesn't work so i added alias sourcing inside .zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,17 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+#cd related aliases
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias ..='cd ..'
+alias ...'cd ../..'
+alias cd-='cd -'
+
+#apt-get
+alias apt='sudo apt-get install -y'
+
+alias rmr='rm -rf'
+
+
---
+++ zsh/ohmyzsh.zsh
@@ -0,0 +1,7 @@
+export ZSH=~/.oh-my-zsh
+source $HOME/.oh-my-zsh/oh-my-zsh.sh
+
+ZSH_THEME="avit"
+
+plugins=(git)
+
